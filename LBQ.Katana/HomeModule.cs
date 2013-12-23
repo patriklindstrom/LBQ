65cd9124355a494290b427565bedede71156f77e
Patrik Lindstrom <patriklindstrom@hotmail.com>
2013-12-23 22:29:01 +0100

    Added Nancy pkg
    Build Works

---
+++ LBQ.Katana/HomeModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace LBQ.Katana
+{
+    internal class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ =>
+            {
+                var model = new {title = "We've Got Issues..."};
+                return View["home", model];
+            };
+        }
+    }
+ 
+}
